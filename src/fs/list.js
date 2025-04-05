16fd9b202ba9940337bff75ba34767e7a9ac4763
AlexShyshko <shish1988@mail.ru>
2025-04-05 22:23:31 +0300

    feat: File system (src/fs) - list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,39 @@
+import { FOLDER_NAMES, ERROR_CONTENT } from './CONST.js';
+import { CHECK_EXISTENSE } from './TOOLS.js';
+import path from 'path';
+import { readdir } from 'fs/promises';
+
 const list = async () => {
-    // Write your code here 
+    
+    try {
+
+        let existentFolderPath = path.join(import.meta.dirname, FOLDER_NAMES.EXISTENT_FILES_FOLDER_NAME);
+        let doesFolderExist = await CHECK_EXISTENSE(existentFolderPath);
+
+        if (!doesFolderExist) {
+
+			let errorOptions = {
+				cause: `Error occured in ${import.meta.filename}. ${existentFolderPath} doesn\'t exist.`,
+			};
+			throw new Error(ERROR_CONTENT.ERROR_MESSAGE, errorOptions);
+
+        } else {
+
+            let filesList = await readdir(existentFolderPath);
+            let reportTitle = `${existentFolderPath} contains ${filesList.length} file(s):`;
+            filesList = filesList.map((file, index) => {
+                return `${++index}) ${file}`;
+            });
+            filesList.unshift(reportTitle);
+            let reportForConsole = filesList.join('\n');
+            console.log(reportForConsole);
+
+        }
+
+    } catch(e) {
+        console.log(e);
+    }
+
 };
 
 await list();
