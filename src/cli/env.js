43ca36ed591f90a81a689fc595a5e961bb580eec
AlexShyshko <shish1988@mail.ru>
2025-04-06 14:38:50 +0300

    feat: Command line interface (src/cli) - env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,13 @@
 const parseEnv = () => {
-    // Write your code here 
+    
+    const regexpTemplate = /^RSS_/;
+    let variablesKeys = Object.keys(process.env);
+    console.log('\x1b[32mThe search for environment variables start with "RSS_" is in progress. See results below:\x1b[0m');
+    variablesKeys.forEach((key) => {
+        regexpTemplate.test(key) ? console.log(`\x1b[45m${key}=${process.env[key]}\x1b[0m`) : null;
+    });
+    console.log('\x1b[33mThe search for environment variables start with "RSS_" has done.\x1b[0m');
+
 };
 
 parseEnv();
