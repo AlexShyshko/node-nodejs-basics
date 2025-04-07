1e342184c756a35204e242630b39bcca46e9180c
AlexShyshko <shish1988@mail.ru>
2025-04-07 14:01:09 +0300

    feat: Streams (src/streams) - write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,33 @@
+import path from 'path';
+import { createWriteStream } from 'fs';
+
 const write = async () => {
-    // Write your code here 
+
+    try {
+
+        let filePath = path.join(import.meta.dirname, 'files', 'fileToWrite.txt');
+        let writeStream = createWriteStream(filePath);
+        console.log(`\x1b[32mType any content below. To exit press CTRL+C (for Windows).\x1b[0m`);
+
+        process.stdin.on('data', (input) => {
+            writeStream.write(input);
+        });
+
+        process.on('SIGINT', () => {
+
+            writeStream.end(() => {
+                
+                console.log(`\x1b[33mOpen the file ${filePath} to see the result.\x1b[0m`);
+                process.exit(0);
+
+            });
+
+        });
+
+    } catch(e) {
+        console.log(`\x1b[31m${e}\x1b[0m`);
+    }
+
 };
 
 await write();
