6f93f6b2abdb96eb4902cde7411fb22bd26c85c3
AlexShyshko <shish1988@mail.ru>
2025-04-07 14:46:45 +0300

    feat: Zlib (src/zip) - compress.js

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,25 @@
+import path from 'path';
+import { createReadStream } from 'fs';
+import { createGzip } from 'zlib';
+import { createWriteStream } from 'fs';
+
 const compress = async () => {
-    // Write your code here 
+    
+    try {
+
+        let sourceFilePath = path.join(import.meta.dirname, 'files', 'fileToCompress.txt');
+        let targetFilePath = path.join(import.meta.dirname, 'files', 'archive.gz');
+        let readStream = createReadStream(sourceFilePath);
+        let fileCompressor = createGzip();
+        let writeStream = createWriteStream(targetFilePath);
+
+        readStream.pipe(fileCompressor).pipe(writeStream);
+        console.log(`\x1b[32mCheck the compressed file ${targetFilePath}\x1b[0m`);
+
+    } catch(e) {
+        console.log(`\x1b[31m${e}\x1b[0m`);
+    }
+
 };
 
 await compress();
