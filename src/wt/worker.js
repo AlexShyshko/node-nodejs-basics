7d3f4414f322bb8beb9f22a538b1cb2856c83003
AlexShyshko <shish1988@mail.ru>
2025-04-08 16:33:59 +0300

    feat: Worker Threads (src/wt) - worker.js

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,27 @@
+import { parentPort } from 'worker_threads';
+
 // n should be received from main thread
 const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 const sendResult = () => {
     // This function sends result of nthFibonacci computations to main thread
+
+    try {
+
+        parentPort.on('message', (number) => {
+
+            let computationResult = nthFibonacci(number);
+            parentPort.postMessage(computationResult);
+
+        })
+
+    } catch(e) {
+
+        console.log(`\x1b[31m${e}\x1b[0m`);
+        parentPort.postMessage(e);
+
+    }
+
 };
 
 sendResult();
